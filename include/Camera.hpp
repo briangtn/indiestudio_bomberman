70b78a509edd7ac25f16f85b296b95107c20013c
BrianGtn <brian.guitteny@epitech.eu>
2019-05-15 14:19:04 +0200

    + Adding camera component

---
+++ include/Camera.hpp
@@ -0,0 +1,14 @@
+//
+// Created by brian on 5/14/19.
+//
+
+#ifndef INDIESTUDIO_CAMERA_HPP
+#define INDIESTUDIO_CAMERA_HPP
+
+
+class Camera {
+
+};
+
+
+#endif //INDIESTUDIO_CAMERA_HPP
---
+++ sources/components/Camera.cpp
@@ -0,0 +1,5 @@
+//
+// Created by brian on 5/14/19.
+//
+
+#include "Camera.hpp"
