9a0457151f983fbe39b2ada38bebc91a5cf0e369
Julian <julian.frabel@epitech.eu>
2019-05-24 14:54:56 +0200

    Added hoverer

--- sources/scenes/StaticTestScene.cpp
+++ sources/scenes/StaticTestScene.cpp
@@ -17,6 +17,7 @@
 #include "components/ComponentParticle.hpp"
 #include "Exceptions.hpp"
 #include "components/Rotator.hpp"
+#include "components/Hoverer.hpp"
 
 void indie::scenes::StaticTestScene::onStart()
 {
@@ -87,6 +88,7 @@ void indie::scenes::StaticTestScene::onStart()
     auto cubeEntity = ecs.entityManager.createEntity("cube");
     auto tr2 = cubeEntity->assignComponent<indie::components::Transform>();
     cubeEntity->assignComponent<indie::components::Rotator, indie::maths::Vector3D>({0, 90, 0});
+    cubeEntity->assignComponent<indie::components::Hoverer, indie::maths::Vector3D, indie::maths::Vector3D>({0, 1, 0}, {0, 1, 0});
     cubeEntity->assignComponent<indie::components::Mesh, std::string>("../test_assets/cube.obj");
     auto mat = cubeEntity->assignComponent<indie::components::Material, std::string>("../test_assets/cube_texture.png");
     mat->setMaterialFlag(irr::video::EMF_BILINEAR_FILTER, false);
--- sources/systems/MovementSystem.cpp
+++ sources/systems/MovementSystem.cpp
@@ -7,10 +7,12 @@
 
 /* Created the 24/05/2019 at 13:53 by jfrabel */
 
+#include <cmath>
 #include "ECSWrapper.hpp"
 #include "systems/MovementSystem.hpp"
 #include "components/Transform.hpp"
 #include "components/Rotator.hpp"
+#include "components/Hoverer.hpp"
 
 indie::systems::MovementSystem::MovementSystem()
 {
@@ -52,7 +54,6 @@ void indie::systems::MovementSystem::updateRotator(const std::chrono::nanosecond
 {
     ECSWrapper ecs;
     float elapsedTimeAsSecond = elapsedTime.count() / 1000000000.0f;
-    std::cout << "ROTATE " << elapsedTimeAsSecond << std::endl;
     ecs.entityManager.applyToEach<components::Transform, components::Rotator>(
         [elapsedTimeAsSecond](jf::entities::EntityHandler entity, jf::components::ComponentHandler<components::Transform> tr, jf::components::ComponentHandler<components::Rotator> rotator) {
             auto rot = tr->getRotation();
@@ -64,5 +65,27 @@ void indie::systems::MovementSystem::updateRotator(const std::chrono::nanosecond
 
 void indie::systems::MovementSystem::updateHoverer(const std::chrono::nanoseconds &elapsedTime) const
 {
-
+    ECSWrapper ecs;
+    float elapsedTimeAsSecond = elapsedTime.count() / 1000000000.0f;
+    ecs.entityManager.applyToEach<components::Transform, components::Hoverer>(
+        [elapsedTimeAsSecond](jf::entities::EntityHandler entity, jf::components::ComponentHandler<components::Transform> tr, jf::components::ComponentHandler<components::Hoverer> hoverer) {
+            auto speed = hoverer->getSpeed();
+            auto amplitude = hoverer->getAmplitude();
+            auto advancement = hoverer->getAdvancement();
+            auto initialPos = tr->getPosition();
+            initialPos.x -= (sinf(advancement.x) * amplitude.x);
+            initialPos.y -= (sinf(advancement.y) * amplitude.y);
+            initialPos.z -= (sinf(advancement.z) * amplitude.z);
+            tr->setPosition({
+                initialPos.x + (sinf(advancement.x + elapsedTimeAsSecond * speed.x) * amplitude.x),
+                initialPos.y + (sinf(advancement.y + elapsedTimeAsSecond * speed.y) * amplitude.y),
+                initialPos.z + (sinf(advancement.z + elapsedTimeAsSecond * speed.z) * amplitude.z)
+            });
+            hoverer->setAdvancement({
+                advancement.x + elapsedTimeAsSecond * speed.x,
+                advancement.y + elapsedTimeAsSecond * speed.y,
+                advancement.z + elapsedTimeAsSecond * speed.z
+            });
+        }
+    );
 }
