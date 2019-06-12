2bf80e53ee38a0aa6d7c59924de61160374349d6
guillaume <guillaume.jeanne@epitech.eu>
2019-06-12 15:11:45 +0200

    AISystem started logic function with apply to each and currently doing choose state functoin

--- include/systems/AISystem.hpp
+++ include/systems/AISystem.hpp
@@ -9,6 +9,7 @@
 #define AISYSTEM_HPP_
 
 #include "System.hpp"
+#include "components/AIController.hpp"
 
 namespace indie {
     
@@ -25,6 +26,10 @@ namespace indie {
                 void onUpdate(const std::chrono::nanoseconds &elapsedTime) override;
                 void onStop() override;
                 void onTearDown() override;
+        
+                static void chooseState(jf::components::ComponentHandler<indie::components::AIController> component);
+                static void AILogic(jf::entities::EntityHandler entity,
+    jf::components::ComponentHandler<indie::components::AIController> component);
             protected:
             private:
                 int _timePassed;
--- sources/systems/AISystem.cpp
+++ sources/systems/AISystem.cpp
@@ -6,8 +6,10 @@
 */
 
 #include "systems/AISystem.hpp"
+#include "components/AIController.hpp"
 #include "events/IrrlichtAnimationEndEvent.hpp"
 #include "ECSWrapper.hpp"
+#include "ai/AiView.hpp"
 
 indie::systems::AISystem::AISystem() : _timePassed(0)
 {
@@ -40,8 +42,28 @@ void indie::systems::AISystem::onTearDown()
 void indie::systems::AISystem::onUpdate(const std::chrono::nanoseconds &elapsedTime)
 {
     _timePassed += elapsedTime.count();
-    if (_timePassed >= 500000000) {
-        _timePassed = 0;
-        //logic here
+    if (_timePassed < 500000000)
+        return;
+    _timePassed = 0;
+
+    ECSWrapper ecs;
+    ecs.entityManager.applyToEach<components::AIController>(&AILogic);
+}
+
+void indie::systems::AISystem::AILogic(jf::entities::EntityHandler entity,
+    jf::components::ComponentHandler<indie::components::AIController> component)
+{
+    if (component->getState() != indie::components::AIController::POWERUP)
+        chooseState(component);
+}
+
+void indie::systems::AISystem::chooseState(jf::components::ComponentHandler<indie::components::AIController> component)
+{
+    ai::AIView::AICellViewGrid grid = ai::AIView::getViewGrid();
+
+    for (auto &row : grid) {
+        for (auto &col : row) {
+            
+        }
     }
 }
