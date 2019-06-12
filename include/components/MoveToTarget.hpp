c576758cf2fdd2f5b3452ac6c0f810f8193d022d
Julian <julian.frabel@epitech.eu>
2019-06-12 12:06:09 +0200

    Added move to target component

---
+++ include/components/MoveToTarget.hpp
@@ -0,0 +1,37 @@
+/*
+** EPITECH PROJECT, 2018
+** indiestudio
+** File description:
+** MoveToTarget.hpp
+*/
+
+/* Created the 12/06/2019 at 11:32 by jfrabel */
+
+#ifndef INDIESTUDIO_MOVETOTARGET_HPP
+#define INDIESTUDIO_MOVETOTARGET_HPP
+
+#include "Component.hpp"
+#include "maths/Vectors.hpp"
+
+namespace indie {
+
+    namespace components {
+
+        class MoveToTarget : public jf::components::Component {
+        public:
+            explicit MoveToTarget(jf::entities::Entity &entity);
+
+            const maths::Vector3D &getTarget() const;
+            void setTarget(const maths::Vector3D &target);
+
+            bool isFollowTarget() const;
+            void setFollowTarget(bool followTarget);
+
+        private:
+            maths::Vector3D _target;
+            bool _followTarget;
+        };
+    }
+}
+
+#endif //INDIESTUDIO_MOVETOTARGET_HPP
